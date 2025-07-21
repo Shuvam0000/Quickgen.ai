0994cce6bcf262e46493dad94c36d803b4a4b97d
Shuvam podder <shuvam2424@gmail.com>
2025-07-21 22:47:12 +0530

    Fix

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,4 +1,5 @@
-import logo from "./logo.svg";
+//import logo from "./logo.svg";
+import logo from "./quickgen.webp"
 import gradientBackground from "./gradientBackground.png";
 import user_group from "./user_group.png";
 import star_icon from "./star_icon.svg";
@@ -19,6 +20,7 @@ export const assets = {
     star_dull_icon,
     profile_img_1,
     arrow_icon,
+    
 };
 
 export const AiToolsData = [
